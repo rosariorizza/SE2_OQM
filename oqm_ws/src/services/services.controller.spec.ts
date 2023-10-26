af47168ccc588dc3c7170e46b5016bf78f28b2b0
rosariorizza <rosariorizza@github.com>
2023-10-26 06:46:40 +0200

    database setup (not completed)

--- oqm_ws/src/services/services.controller.spec.ts
+++ oqm_ws/src/services/services.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('ServicesController', () => {
   let controller: ServicesController;
@@ -9,6 +11,35 @@ describe('ServicesController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ServicesController],
       providers: [ServicesService],
+
+
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: [
+            `env/${process.env.NODE_ENV || 'local'}.env`,
+            'env/default.env',
+          ],
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
+            type: 'postgres',
+            host: configService.get('HOST_DB'),
+            port: +configService.get('PORT_DB'),
+            username: configService.get('USERNAME_DB'),
+            password: configService.get('PASSWORD_DB'),
+            database: configService.get('NAME_DB'),
+            logging: configService.get('TYPEORM_LOGGING'),
+            entities: [configService.get('TYPEORM_ENTITIES')],
+            synchronize: configService.get('TYPEORM_SYNC'),
+            migrations: [__dirname + '/migrations/*{.ts,.js}'],
+            migrationsRun: true,
+          }),
+          inject: [ConfigService],
+        }),
+      ],
+      
     }).compile();
 
     controller = module.get<ServicesController>(ServicesController);
--- oqm_ws/test/app.e2e-spec.ts
+++ oqm_ws/test/app.e2e-spec.ts
@@ -2,23 +2,59 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServicesModule } from 'src/services/services.module';
 
-describe('AppController (e2e)', () => {
+describe('Service controller (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ServicesModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: [
+            `env/${process.env.NODE_ENV || 'local'}.env`,
+            'env/default.env',
+          ],
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
+            type: 'postgres',
+            host: configService.get('HOST_DB'),
+            port: +configService.get('PORT_DB'),
+            username: configService.get('USERNAME_DB'),
+            password: configService.get('PASSWORD_DB'),
+            database: configService.get('NAME_DB'),
+            logging: configService.get('TYPEORM_LOGGING'),
+            entities: [configService.get('TYPEORM_ENTITIES')],
+            synchronize: configService.get('TYPEORM_SYNC'),
+            migrations: ['/home/rosario/softeng2/SE2_OQM/oqm_ws/migrations/services.sql'],
+            migrationsRun: true,
+          }),
+          inject: [ConfigService],
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
   });
+
+  afterEach(()=>{
+
+  })
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api/services')
       .expect(200)
-      .expect('Hello World!');
+      .expect([{description: 'sample description for customer support service', type: 'CUSTOMER_SUPPORT', time: 10},
+      {description: 'sample description for technical assistance service', type: 'TECHNICAL_ASSISTANCE', time: 5}]);
   });
 });
